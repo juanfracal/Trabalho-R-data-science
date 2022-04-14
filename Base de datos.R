6eb5d312ff5f649dad790bc2fa8007862c4fa5da
renan-olivier <103616269+renan-olivier@users.noreply.github.com>
2022-04-14 18:55:41 -0300

    Update Base de datos.R

--- Base de datos.R
+++ Base de datos.R
@@ -16,18 +16,16 @@ library(tidyverse)
 dt <- "2017-01-01"
 df <- "2022-04-01"
 
-# INDEX VALUES SINCE 2010 
-#IBOVESPA (OK)
+# INDEX VALUES SINCE 2017 
+#IBOVESPA
 getSymbols("^BVSP" ,from =dt , to = df, auto.assign= F)
 #NASDAQ (No caso o SP500, composto pelas 500 maioresA empresas listadas na bolsa de NY)
 getSymbols("^GSPC" , from =dt, to = df, auto.assign= F)
 #CMC Crypto 200 Index (2019)
 getSymbols("^CMC200" , from =dt , to = df, auto.assign= F)
-#Mirae Asset Renda Fixa Pre Fundo De Indice (2018)
-getSymbols("FIXA11.SA" , from =dt , to = df, auto.assign= F)
-# Ouro futuro (OK)
+# Ouro futuro
 getSymbols("GC=F" ,from =dt, to = df, auto.assign= F)
-#Bitcoin (#2014
+#Bitcoin 
 getSymbols("BTC-USD" ,from =dt, to = df, auto.assign= F)
 #Selic (Usando APIs do Banco Central)
 url_selic <- paste0('http://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=csv&dataInicial=',
