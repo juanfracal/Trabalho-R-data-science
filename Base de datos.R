c57cb4bed6e0491ee809ca30fe0b0589c6d65b73
juanfracal <103221694+juanfracal@users.noreply.github.com>
2022-04-14 11:21:07 -0300

    Update Base de datos.R
    bitcoin

--- Base de datos.R
+++ Base de datos.R
@@ -13,10 +13,13 @@ getSymbols("^CMC200" , from = " 2010-01-01", to = "2022-04-01")
 getSymbols("FIXA11.SA" , from = " 2010-01-01", to = "2022-04-01")
 # Ouro futuro
 getSymbols("GC=F" ,from = " 2010-01-01", to = "2022-04-01")
+#Bitcoin 
+getSymbols("BTC-USD" ,from = " 2010-01-01", to = "2022-04-01")
 
-#Gráficos: 
+
 chartSeries(BVSP)
 chartSeries(IXIC)
 chartSeries(CMC200)
 chartSeries(FIXA11.SA)
 chartSeries(GC=F)
+chartSeries(BTC-USD)
