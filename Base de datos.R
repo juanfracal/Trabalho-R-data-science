55e5eddfa1d990ec9b3976a348f080306cc19f75
renan-olivier <103616269+renan-olivier@users.noreply.github.com>
2022-04-14 14:10:40 -0300

    Update Base de datos.R

--- Base de datos.R
+++ Base de datos.R
@@ -5,17 +5,17 @@ library(quantmod)
 dt <- "2010-01-01"
 
 # INDEX VALUES SINCE 2010 
-#IBOVESPA
+#IBOVESPA (OK)
 getSymbols("^BVSP" ,from = " 2010-01-01", to = "2022-04-01", auto.assign= F)
-#NASDAQ
-getSymbols("^IXIC" , from = " 2010-01-01", to = "2022-04-01", auto.assign= F)
-#CMC Crypto 200 Index
+#NASDAQ (No caso o SP500, composto pelas 500 maioresA empresas listadas na bolsa de NY)
+getSymbols("^GSPC" , from = " 2010-01-01", to = "2022-04-01", auto.assign= F)
+#CMC Crypto 200 Index (2019)
 getSymbols("^CMC200" , from = " 2010-01-01", to = "2022-04-01", auto.assign= F)
-#Mirae Asset Renda Fixa Pre Fundo De Indice
+#Mirae Asset Renda Fixa Pre Fundo De Indice (2018)
 getSymbols("FIXA11.SA" , from = " 2010-01-01", to = "2022-04-01", auto.assign= F)
-# Ouro futuro
+# Ouro futuro (OK)
 getSymbols("GC=F" ,from = " 2010-01-01", to = "2022-04-01", auto.assign= F)
-#Bitcoin 
+#Bitcoin (#2014)
 getSymbols("BTC-USD" ,from = " 2010-01-01", to = "2022-04-01", auto.assign= F)
 
 
