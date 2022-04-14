8e6eb84e772119781a0a9a8d2beb587987c440ac
guilherme123 <103221720+beterrabanuclear@users.noreply.github.com>
2022-04-13 23:43:30 -0300

    Update Base de datos.R

--- Base de datos.R
+++ Base de datos.R
@@ -5,13 +5,18 @@ library(quantmod)
 # INDEX VALUES SINCE 2010 
 #IBOVESPA
 getSymbols("^BVSP" ,from = " 2010-01-01", to = "2022-04-01")
+chartSeries(BVSP)
 #NASDAQ
 getSymbols("^IXIC" , from = " 2010-01-01", to = "2022-04-01")
+chartSeries(IXIC)
 #CMC Crypto 200 Index
 getSymbols("^CMC200" , from = " 2010-01-01", to = "2022-04-01")
+chartSeries(CMC200)
 #Itau FIC De Fundos De Investimento Em Direitos Creditorios De Infraestrutura
 getSymbols("IFRA11.SA" , from = " 2010-01-01", to = "2022-04-01")
 #Mirae Asset Renda Fixa Pre Fundo De Indice
 getSymbols("FIXA11.SA" , from = " 2010-01-01", to = "2022-04-01")
+chartSeries(FIXA11.SA)
 # Ouro futuro
 getSymbols("GC=F" ,from = " 2010-01-01", to = "2022-04-01")
+chartSeries(GC=F)
